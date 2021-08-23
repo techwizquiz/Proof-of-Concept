555a5a8b44731283005c107dda8819c8b56fa7dc
cmmerritt <christiane.merritt@gmail.com>
2021-08-23 16:33:45 -0700

    dynamically render random question and answers to page

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import questionRandomizer from '../state/questionRandomizer';
+import { questions } from '../resources/QuestionPool';
 
 // on load, choose a random question
 // display question and answers
@@ -8,20 +9,41 @@ import questionRandomizer from '../state/questionRandomizer';
 // if correct, they get one point added to score and new question loads
 // if incorrect, new question loads
 
-export default class Questions extends Component {
-  render() {
-    return (
-      <div>
-        <p>Level: {level}</p>       
-        <p>Question: {question}</p>       
+const Questions = () => {
+  const [question, setQuestion] = useState(questionRandomizer(questions));
+
+  console.log(question);
+
+  return (
+    <div>       
+      <p>Question: {question.question}</p>       
+      
+      <div>       
+        <label><input type="radio" name="answer" value="A" /> A: {question.choiceA}</label>
+        <label><input type="radio" name="answer" value="B"/>B: {question.choiceB}</label>   
+        <label><input type="radio" name="answer" value="C"/>C: {question.choiceC}</label>       
+        <label><input type="radio" name="answer" value="D"/>D: {question.choiceD}</label>
+      </div>       
+    </div>
+  );
+};
+
+export default Questions; 
+
+// export default class Questions extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <p>Level: {level}</p>       
+//         <p>Question: {question}</p>       
         
-        <div>       
-          <input type="radio" name="answer" value="A">A: {choiceA}</input>       
-          <input type="radio" name="answer" value="B">B: {choiceB}</input>       
-          <input type="radio" name="answer" value="C">C: {choiceC}</input>       
-          <input type="radio" name="answer" value="D">D: {choiceD}</input>
-        </div>       
-      </div>
-    );
-  }
-}
+//         <div>       
+//           <input type="radio" name="answer" value="A">A: {choiceA}</input>       
+//           <input type="radio" name="answer" value="B">B: {choiceB}</input>       
+//           <input type="radio" name="answer" value="C">C: {choiceC}</input>       
+//           <input type="radio" name="answer" value="D">D: {choiceD}</input>
+//         </div>       
+//       </div>
+//     );
+//   }
+// }
