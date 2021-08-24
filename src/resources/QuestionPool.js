10880bb702366c16d701c0e23d5b0b7b58850dee
taylor-tokareff <taylortokareff@gmail.com>
2021-08-24 10:12:56 -0700

    solved questions and answers state updating properly

--- src/resources/QuestionPool.js
+++ src/resources/QuestionPool.js
@@ -4,7 +4,7 @@ export const questions = [
   {
     id: 1,
     question: 'What is node?',
-    answer: 'open-source, cross-platform, back-end JavaScript runtime environment',
+    answer: 'D',
     choiceA: 'a theme extension for VSCode',
     choiceB: 'its own language',
     choiceC: 'an alternative to CRUD routes',
@@ -19,7 +19,7 @@ export const questions = [
         var name = 'Lydia';
         let age = 21;
       }`,
-    answer: 'undefined and ReferenceError',
+    answer: 'D',
     choiceA: 'Lydia and undefined',
     choiceB: 'Lydia and ReferenceError',
     choiceC: 'ReferenceError and 21',
@@ -35,7 +35,7 @@ export const questions = [
       for (let i = 0; i < 3; i++) {
         setTimeout(() => console.log(i), 1);
       }`,
-    answer: '3 3 3 and 0 1 2',
+    answer: 'C',
     choiceA: '0 1 2 and 0 1 2',
     choiceB: '0 1 2 and 3 3 3',
     choiceC: '3 3 3 and 0 1 2',
